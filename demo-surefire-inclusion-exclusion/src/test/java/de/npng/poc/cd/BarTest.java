17601b43b4dc9f2d6b93e3e15e340573e9faba3f
Benjamin Herbert <bherbert@trustinternational.com>
2014-04-10 18:18:52 +0200

    Adding example for exclusion and inclusion for unit tests & integration tests

---
+++ demo-surefire-inclusion-exclusion/src/test/java/de/npng/poc/cd/BarTest.java
@@ -0,0 +1,11 @@
+package de.npng.poc.cd;
+
+import org.junit.Test;
+
+public class BarTest {
+
+	@Test
+	public void test() {
+	}
+
+}
---
+++ demo-surefire-inclusion-exclusion/src/test/java/de/npng/poc/cd/integration/FuuTest.java
@@ -0,0 +1,11 @@
+package de.npng.poc.cd.integration;
+
+import org.junit.Test;
+
+public class FuuTest {
+
+	@Test
+	public void test() {
+	}
+
+}
